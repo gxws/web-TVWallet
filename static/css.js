2488ffbfd1147a21fa780cb6a77922bf8ed15d97
jqjjian-pc <jqjjian@126.com>
2015-10-15 09:38:22 +0800

    20151015
    20151015

--- static/css.js
+++ static/css.js
@@ -1,8 +1,9 @@
 return {
 	files: {
 		/* begin 基础模块 */
-		'{static}/TVWallet/static/css/base.mod.css': [
-			'{static}/TVWallet/static/css/base.min.css'
+		'{static}/web-TVWallet/static/css/base.mod.css': [
+			'{static}/web-TVWallet/static/css/base.min.css',
+			'{static}/web-TVWallet/static/css/base_HD.min.css'
 		]
 		/* end 基础模块 */
 	}
